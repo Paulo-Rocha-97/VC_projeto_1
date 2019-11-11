3de9178814dde0b8d4a4176159af7b99271d9836
Paulo-Rocha-97 <57599652+Paulo-Rocha-97@users.noreply.github.com>
2019-11-11 16:00:15 +0000

    *Update

---
+++ Toolbox_VC/SLIC.m
@@ -0,0 +1,27 @@
+% Primeira utilização do algoritmo SLIC
+% imread - constroi uma matriz de indices através de cada pixel
+% ind2rgb - passar do imread para RGB
+% If cmap empty => RGB values are store for pixel format (true color format)
+
+
+function [outputImage] = SLIC(number_spixel,A)
+
+[L,N] = superpixels(A,number_spixel);
+
+BW = boundarymask(L);
+outputImage = zeros(size(A),'like',A);
+idx = label2idx(L);
+numRows = size(A,1);
+numCols = size(A,2);
+for labelVal = 1:N
+    redIdx = idx{labelVal};
+    greenIdx = idx{labelVal}+numRows*numCols;
+    blueIdx = idx{labelVal}+2*numRows*numCols;
+    outputImage(redIdx) = mean(A(redIdx));
+    outputImage(greenIdx) = mean(A(greenIdx));
+    outputImage(blueIdx) = mean(A(blueIdx));
+end    
+
+% subplot(2,1,2), imshow(imoverlay(A,BW,'black'))
+
+end
