f738943f277a8fee3b6b165e7a75318840d865bd
Nogueira-hub <57599659+Nogueira-hub@users.noreply.github.com>
2019-11-22 15:54:01 +0000

    *Árvores e Cores

--- Toolbox_VC/Colour_Segmentation.m
+++ Toolbox_VC/Colour_Segmentation.m
@@ -1,3 +1,5 @@
+function [Verde] = Colour_Segmentation
+
 A = imread('VC_P1_7.JPG');
 
 A = histeq(A);
@@ -10,7 +12,7 @@ H_A = A(:,:,1);
 S_A = A(:,:,2);
 V_A = A(:,:,3);
 
-delta_x = 0.10;
+delta_h = 0.10;
 %Separar por cores: Amarelo, Azul, Verde
 % H azul = 2/3; amarelo = 1/6; verde = 1/3
 
@@ -20,19 +22,19 @@ Verde = zeros(comp_m,larg_m);
 Amarelo = zeros(comp_m,larg_m);
 for i = 1:comp_m
     for j = 1:larg_m        
-        if H_A(i,j) <= 2/3 + delta_x & H_A(i,j) >= 2/3 - delta_x
+        if H_A(i,j) <= 2/3 + delta_h & H_A(i,j) >= 2/3 - delta_h
             Azul(i,j) = 1;
         else
             Azul(i,j) = 0;
         end
         
-        if H_A(i,j) <= 1/3 + delta_x & H_A(i,j) >= 1/3 - delta_x
+        if H_A(i,j) <= 1/3 + delta_h && H_A(i,j) >= 1/3 - delta_h
             Verde(i,j) = 1;
         else
             Verde(i,j) = 0;
         end
         % Tentar que amarelo não apareça
-        if H_A(i,j) <= 1/6 + delta_x & H_A(i,j) >= 1/6 - delta_x
+        if H_A(i,j) <= 1/6 + delta_h && H_A(i,j) >= 1/6 - delta_h
             Amarelo(i,j) = 1;
         else
             Amarelo(i,j) = 0;
@@ -42,27 +44,44 @@ end
 
 
 figure(1)
-subplot(3,3,1)
+subplot(1,3,1)
 imshow(Azul)
 title('Azul')
-subplot(3,3,2)
+subplot(1,3,2)
 imshow(Verde)
 title('Verde')
-subplot(3,3,3)
+subplot(1,3,3)
 imshow(Amarelo)
 title('Amarelo')
             
-stropen = strel('disk',10);
-strclose = strel('octagon',9);
+stropen = strel('disk',1);
+strclose = strel('disk',5);
+strdilate = strel('disk',3);
 
-%Segmentação da zona verde
-% Verde_Open = imopen(Verde,stropen);
-% figure(3)
-% imshow(Verde_Open)
-% 
-% Verde_Close = imclose(Verde_Open,strclose);
-% figure(4)
-% imshow(Verde_Close)
+for i = 1:comp_m
+    for j = 1:larg_m
+        if V_A(i,j) >= 0.35 && S_A(i,j) >= 0.25 && Verde(i,j) == 1
+            Arvores(i,j) = 1;
+        else
+            Arvores(i,j) = 0;
+        end
+    end
+end
+figure(3)
+imshow(Arvores)
+% Segmentação da zona verde
+
+Arvores_Open = bwareaopen(Arvores,8);
+figure(4)
+imshow(Arvores_Open)
+
+Arvores_Close = imclose(Arvores_Open,strclose);
+figure(5)
+imshow(Arvores_Close)
+
+Arvores_Final = imdilate(Arvores_Close,strdilate);
+figure(6)
+imshow(Arvores_Final)
 % 
 % Verde_final = bwareaopen(Verde_Close,100);
 % figure(5)
