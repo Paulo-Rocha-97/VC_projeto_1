ddb5b8e538c902d58dc5f0dd9148c3168bc25cb4
Paulo-Rocha-97 <57599652+Paulo-Rocha-97@users.noreply.github.com>
2019-11-22 16:19:18 +0000

    *Update

--- VC_project_1.m
+++ VC_project_1.m
@@ -92,6 +92,7 @@ for i = 4:size(S)
 end  
 
 mystring = sprintf(mystring);
+
 set(handles.List_of_images, 'String', mystring);
 
 
@@ -108,8 +109,6 @@ function file_select_text_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of file_select_text as text
-%        str2double(get(hObject,'String')) returns contents of file_select_text as a double
 
 
 % --- Executes during object creation, after setting all properties.
@@ -134,7 +133,8 @@ function Preview_Callback(hObject, eventdata, handles)
 local = cd;
 D=strcat(local,'\Images');
 S = dir(fullfile(D));
- 
+
+
 file = get(handles.file_select_text,'String');
 
 cont=0;
@@ -153,6 +153,7 @@ if cont==1
     name = fullfile(D,S(number).name);
         
     A = imread(name);
+    
     set(handles.image_display,'Units','pixels');
     imshow(A,'Parent', handles.image_display);
 
@@ -166,7 +167,9 @@ function next_stage_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+
 set(handles.Done,'Visible','off')
+set(handles.Warning,'Visible','on')
 
 local = cd;
 D=strcat(local,'\Images');
@@ -191,6 +194,7 @@ if cont==1
     name = fullfile(D,S(number).name);
         
     A = imread(name);
+    image_display = findobj(0,'tag','image_display');
     set(handles.image_display,'Units','pixels');
     imshow(A,'Parent', handles.image_display);
     
@@ -221,7 +225,6 @@ if cont==1
     save('original_image.mat', 'OG' )
     
     % Hide and display buttons
-    
     set(handles.Get_filenames,'Visible','off')
     set(handles.List_of_images,'Visible','off')
     set(handles.next_stage,'Visible','off')
@@ -306,11 +309,13 @@ function Road_detection_Callback(hObject, eventdata, handles)
 % hObject    handle to Road_detection (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
 close all
 set(handles.Done,'Visible','off')
 set(handles.More_sand,'Visible','off')
 set(handles.More_water,'Visible','off')
 set(handles.More_tree,'Visible','off')
+
 
 load('original_image.mat')
 
@@ -326,6 +331,7 @@ set(handles.More_road,'Visible','on')
 set(handles.Done,'Visible','on')
 
 
+
 % --- Executes on button press in Tree_identification.
 function Tree_identification_Callback(hObject, eventdata, handles)
 % hObject    handle to Tree_identification (see GCBO)
@@ -336,6 +342,7 @@ set(handles.Done,'Visible','off')
 set(handles.More_sand,'Visible','off')
 set(handles.More_water,'Visible','off')
 set(handles.More_road,'Visible','off')
+
 
 load('original_image.mat')
 
@@ -359,6 +366,8 @@ function Back_Callback(hObject, eventdata, handles)
     set(handles.next_stage,'Visible','on')
     set(handles.Preview,'Visible','on')
     set(handles.file_select_text,'Visible','on')
+    set(handles.Warning,'Visible','off')
+
     
     set(handles.Area_Segmentation,'Visible','off')
     set(handles.Sand_detection,'Visible','off')
@@ -413,6 +422,7 @@ function More_water_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 set(handles.Done,'Visible','off')
+set(handles.Warning,'Visible','off')
 
 load('original_image.mat')
 
@@ -441,6 +451,8 @@ function More_road_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 set(handles.Done,'Visible','off')
+set(handles.Warning,'Visible','off')
+
 
 load('original_image.mat')
 
